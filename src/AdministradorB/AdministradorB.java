93b3d7a9263cd3edbd5a5272ac9bf1fe6d23a333
maumanzano <maumanzano@unicauca.edu.co>
2018-05-05 12:25:02 -0500

    Se agregan clases Administrador y usuario

---
+++ src/AdministradorB/AdministradorB.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AdministradorB;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class AdministradorB {
+    
+    private String login;
+    private String clave;
+
+    public AdministradorB() {
+    }
+
+    public AdministradorB(String login, String clave) {
+        this.login = login;
+        this.clave = clave;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+}
