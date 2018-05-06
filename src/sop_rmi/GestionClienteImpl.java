6c8a23c9615309d88c05eb40e13849f9f9d53f38
maumanzano <maumanzano@unicauca.edu.co>
2018-05-05 23:02:55 -0500

    prueba commint

---
+++ src/sop_rmi/GestionClienteImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sop_rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class GestionClienteImpl extends UnicastRemoteObject implements GestionClienteInt {
+
+    @Override
+    public boolean IngresoUsuario(String codigo) throws RemoteException {
+        boolean flag=false;
+        
+        return flag;
+    }
+
+    @Override
+    public boolean SalidaUsuario(String codigo) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
